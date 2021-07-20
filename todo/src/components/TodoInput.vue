2898c57891952401750f532aec97a4a6e87bce8b
이용우 <dyddn0823@hanyang.ac.kr>
2021-07-21 00:15:28 +0900

    feat: TodoInput에 Bootstrap 적용

--- todo/src/components/TodoInput.vue
+++ todo/src/components/TodoInput.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <input type="text" placeholder="할 일을 입력하세요" maxlength="200" v-model="input">
-    <button @click.prevent="onClickButton">등록</button>
+    <b-input-group style="width: 50%">
+      <b-form-input placeholder="할 일을 입력하세요" maxlength="200" v-model="input"></b-form-input>
+      <b-input-group-append>
+        <b-button @click.prevent="onClickButton">등록</b-button>
+      </b-input-group-append>
+    </b-input-group>
   </div>
 </template>
 
